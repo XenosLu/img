b740990ba4a6aa0836241c1e047a5cb343675eb4
Xenocider <xenos.lu@gmail.com>
2022-04-20 10:03:37 +0800

    one

---
+++ one/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# echo "$URL"
+cd $(mktemp -d)
+
+
+python3 -m xenoslib.onedrive login $*
+
+if [ "" == "$URL" ]
+then
+    python3 -m xenoslib.onedrive download "/media/upload/conf/task.sh"
+    sh task.sh
+else youtube-dl "$URL" 
+fi
+
+ls -lh
+
+python3 -m xenoslib.onedrive upload . --folder "/media/upload"
+python3 -m xenoslib.onedrive logout
